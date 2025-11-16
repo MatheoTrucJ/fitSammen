43bbf7fb7c719057c0d54a6b81bf1c9c65856a9d
LauritsVN <10632103@ucn.dk>
2025-11-16 17:23:21 +0100

    Add class carousel to homepage and update profile link
    Introduced a new section on the Home page displaying a list of classes in a horizontal carousel with navigation arrows. Updated the profile icon in the layout to link to the Profile page. Added related CSS for the carousel and improved menu/profile styling. Added ServiceUrlToUse to appsettings.json. Began JS implementation for carousel scrolling.

--- FitSammen/FitSammenWebClient/wwwroot/js/site.js
+++ FitSammen/FitSammenWebClient/wwwroot/js/site.js
@@ -2,3 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.querySelector('hold-liste')
+track.scrollBy({ left: +amount, behavior: 'smooth' });
+document.querySelector('hold-liste').scrollBy({ left: +amount, behavior: 'smooth' });
