5a3d3bbd4c1ba66b4027b1f40cc05649460b5d54
Omid15dk <10632132@ucn.dk>
2025-11-26 10:01:31 +0100

    Add location service layer and base service connection
    Introduced ILocationService interface and LocationService class to support location-related operations. Added ServiceConnection abstract class to centralize HTTP communication logic. Also fixed a namespace typo in ClassService.cs.

---
+++ FitSammen/FitSammenDekstopClient/ServiceLayer/ILocationService.cs
@@ -0,0 +1,16 @@
+﻿using FitSammenDekstopClient.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitSammenDekstopClient.ServiceLayer
+{
+    public interface ILocationService
+    {
+        Task<IEnumerable<Location>?> GetAllLocationsAsync();
+        Task<IEnumerable<Room>?> GetAllRoomsFromLocationIdAsync(int locationId);
+        Task<IEnumerable<Employee>?> GetAllEmployeesFromLocationIdAsync(int locationId);
+    }
+}
---
+++ FitSammen/FitSammenDekstopClient/ServiceLayer/LocationService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitSammenDekstopClient.ServiceLayer
+{
+    internal class LocationService : ServiceConnection, ILocationService
+    {
+    }
+}
